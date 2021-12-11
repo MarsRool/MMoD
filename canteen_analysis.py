f00f81e7ad8a5b1c0df1605c7fdfe2fb58f48682
Mars <max.rez@list.ru>
2021-12-11 04:30:41 +0300

    graph fixed as 2 processes combination

--- canteen_analysis.py
+++ canteen_analysis.py
@@ -1,4 +1,5 @@
 import math
+import numpy as np
 from functools import reduce
 from numpy import Inf
 from tabulate import tabulate
@@ -27,7 +28,6 @@ def calculate_empirical_characteristics(canteen):
         if (p_i < 0.001):
             break
         i = i + 1
-    print(f'test {len(p)}')
 
     Q = 1
     A = canteen.X
@@ -42,17 +42,23 @@ def calculate_empirical_characteristics(canteen):
 # Calculate Theoretical Characteristics
 def calculate_theoretical_characteristics(X, mu, q, t):
     alpha = get_canteen_alpha(X, mu, q)
+    p1=[]
+    p2=[]
     p=[]
     Q = None
     A = None
+    m_eating = X * (1 + q) * t
     if canteen_has_static_state(X, mu, q):
-        i = 0
-        while(True):
-            p_i = (alpha ** i) * (1 - alpha)
+        for i in range(15):
+            p1.append((alpha ** i) * (1 - alpha))
+            # p2.append(1 - np.exp(- i / m_eating))
+            # p2.append(1 - (alpha ** i) / math.factorial(i) * np.exp(-1 / m_eating))
+            p2.append(0 if i == 0 else np.exp(- (i - 1) / m_eating) - np.exp(- i / m_eating))
+            indexes_i = [(j, i - j) for j in range(i + 1)]
+            p_i = 0
+            for j in range(len(indexes_i)):
+                p_i = p_i + p1[indexes_i[j][0]] * p2[indexes_i[j][1]]
             p.append(p_i)
-            if (p_i < 0.001):
-                break
-            i = i + 1
         Q = 1
         A = X
         L_canteen = alpha / (1 - alpha) + X * (1 + q) * t
@@ -76,7 +82,7 @@ def display_characteristics(theoretical_characteristics, empirical_characteristi
     p1, Q1, A1, L_canteen1, L_queue1, t_canteen1, t_queue1 = theoretical_characteristics
     p2, Q2, A2, L_canteen2, L_queue2, t_canteen2, t_queue2 = empirical_characteristics
 
-    output_list = [[f'p{(i)}', p1[i], p2[i]] for i in range (min([len(p1), len(p2), 15]))]
+    output_list = [[f'p{(i)}', p1[i], p2[i]] for i in range (min([len(p1), len(p2), 10]))]
     output_list.extend([['Q (относительная пропускная способность)', Q1, Q2],
                         ['A (абсолютная пропускная способность)', A1, A2],
                         ['L СМО (среднее число посетителей в столовой)', L_canteen1, L_canteen2],
@@ -89,7 +95,7 @@ def display_characteristics(theoretical_characteristics, empirical_characteristi
 
 
 def get_xi_2(o, e):
-    return sum(((o[i] - e[i]) ** 2) / e[i] for i in range(len(o)))
+    return sum((0 if e[i] == 0 else ((o[i] - e[i]) ** 2) / e[i]) for i in range(min(len(o), len(e))))
 
 
 def plot_graphs(theoretical_characteristic, empirical_characteristic):
@@ -98,3 +104,18 @@ def plot_graphs(theoretical_characteristic, empirical_characteristic):
     plt.legend(['Теоретические вероятности', 'Эмпирические вероятности'])
     plt.xlabel('p[i]')
     plt.show()
+
+# test = list({sum(c) for i in range(1, len(x) + 1) for c in combinations(x, i)})
+# print(test)
+
+# print(list(combinations(x, 2)))
+
+# def test(sum):
+    # return [(i, sum - i) for i in range(sum + 1)]
+
+# print(test(0))
+# print(test(1))
+# print(test(2))
+# print(test(3))
+# print(test(4))
+# print(test(5))
